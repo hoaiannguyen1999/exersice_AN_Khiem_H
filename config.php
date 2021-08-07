02ea425e0aad882c57dc29df9acbefea0934d7bd
Your Name <an@example.com>
2021-08-07 15:23:49 +0700

    final version

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// ket noi database
+// Connect to database
+$server = "localhost";
+$user = "An";
+$password = "Anmysql99@";
+$dbname = "final_project";
+
+$conn = mysqli_connect($server,$user,$password,$dbname);
+
+?>
